ca72a3080c90762a158b9e654b19da408af206ad
webofceco <mapleselijah9@gmail.com>
2023-12-14 20:08:49 -0500

    test

--- src/Dash/App.jl
+++ src/Dash/App.jl
@@ -248,6 +248,7 @@ app.layout = html_div() do
     html_div(id="footer") do
         html_div() do
             html_a(
+                id="github link",
                 "STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies © 2023 Ceco Elijah Maples",
                 href="https://github.com/cecoeco/STORMS.jl"
 
