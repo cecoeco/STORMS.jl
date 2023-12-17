2227059137db9edad7ab419bb82f62279451b577
webofceco <mapleselijah9@gmail.com>
2023-12-17 17:03:04 -0500

    footer

--- src/app/footer.jl
+++ src/app/footer.jl
@@ -1,9 +1,1 @@
-using Dash
-
-footer = html_footer([
-    html_small([
-        html_img(src = "", alt = "github logo"),
-        html_a("STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies", href = "https://github.com/cecoeco/STORMS.jl"),
-        " © 2023 Ceco Elijah Maples"
-    ])
-])
+using Dash; footer = html_footer([html_small([html_img(src = "", alt = "github logo"), html_a("STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies", href = "https://github.com/cecoeco/STORMS.jl"), " © 2023 Ceco Elijah Maples"])])
