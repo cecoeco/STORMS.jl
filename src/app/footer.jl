5fcad09911e31b3eeff527cceaebc54b931f39c1
webofceco <mapleselijah9@gmail.com>
2023-12-17 17:00:51 -0500

    img

--- src/app/footer.jl
+++ src/app/footer.jl
@@ -2,10 +2,8 @@ using Dash
 
 footer = html_footer([
     html_small([
-        html_a(
-            "STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies",
-            href = "https://github.com/cecoeco/STORMS.jl"
-        ),
+        html_img(src = "", alt = "github logo"),
+        html_a("STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies", href = "https://github.com/cecoeco/STORMS.jl"),
         " © 2023 Ceco Elijah Maples"
     ])
 ])
--- src/app/pages/home/home.jl
+++ src/app/pages/home/home.jl
@@ -2,11 +2,38 @@ using Dash
 
 include("../../../app/footer.jl")
 
-homeLayout = html_div() do
-    html_div(id = "title box") do
-        html_h1(id = "title") do
+homeLayout = html_div(
+    [
+        html_h1(
             "STORMS.jl: Strengthening The Organization and Reporting of Microbiome Studies"
-        end
-    end
-    footer
-end
+        ),
+        html_p(
+            "STORMS description"
+        ),
+        html_h2(
+            "Checklist"
+        ),
+        html_p(
+            "Checklist description"
+        ),
+        html_h2(
+            "Flowcharts"
+        ),
+        html_p(
+            "Flowchart description"
+        ),
+        html_h3(
+            "Observational"
+        ),
+        html_p(
+            "Observational description"
+        ),
+        html_h3(
+            "Experimental"
+        ),
+        html_p(
+            "Experimental description"
+        ),
+        footer
+    ]
+)
