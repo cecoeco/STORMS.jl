34eb7be4e9b04f78e021bcea897e665c40919c22
webofceco <mapleselijah9@gmail.com>
2023-12-20 00:16:58 -0500

    options complete

--- app/pages/flowcharts/Experimental.jl
+++ app/pages/flowcharts/Experimental.jl
@@ -129,5 +129,100 @@ experimentalLayout = html_div([
             ),
         ],
     ),
+    html_div(
+        id = "inputs-container-experimental",
+        [
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "expression of interest"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text", 
+                    placeholder = "assessment for eligibility:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "assessment for eligibility:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "screened"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text", 
+                    placeholder = "excluded:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "excluded:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "recruited and randomized"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "group 1"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "group 2"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text",
+                    placeholder = "drop out:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "drop out:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "completed trial"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "lack of sequencing"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "low number of reads"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "total samples included in the analysis"
+                )
+            )
+        ]
+    ),
     html_div([dcc_graph(figure = experimental())]),
 ])
--- app/pages/flowcharts/Observational.jl
+++ app/pages/flowcharts/Observational.jl
@@ -129,5 +129,88 @@ observationalLayout = html_div([
             ),
         ],
     ),
+    html_div(
+        id = "inputs-container-observational",
+        [
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "expression of interest"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text", 
+                    placeholder = "assessment for eligibility:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "assessment for eligibility:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "screened"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text", 
+                    placeholder = "excluded:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "excluded:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "recruited"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "text", 
+                    placeholder = "drop out:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "drop out:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "data available for analysis:"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "lack of sequencing"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "low number of reads"
+                )
+            ),
+            html_div(
+                dcc_input(
+                    type = "number", 
+                    placeholder = "total samples included in the analysis"
+                )
+            )
+        ]
+    ),
     html_div([dcc_graph(figure = observational())]),
 ])
