437db2b2c6d73ad505d27a6b50013b67c5494e21
webofceco <mapleselijah9@gmail.com>
2023-12-23 05:00:00 -0500

    app

--- app/footer.jl
+++ app/footer.jl
@@ -6,7 +6,7 @@ footer = html_footer(
     className = "footer",
     children = [
         html_img(
-            src = "/Users/cecomaples/Desktop/STORMS.jl/app/assets/img/png/github-mark.png",
+            src = "https://github.com/cecoeco/STORMS.jl/blob/caee1fd4bc780000d265f3c72666bab5e7efb660/app/assets/img/png/github-mark.png",
             alt = "github logo",
             className = "footer-logo"
         ),
